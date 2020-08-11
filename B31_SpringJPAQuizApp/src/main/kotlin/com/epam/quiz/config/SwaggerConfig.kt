d4ffe085f440c48b0f58b958990c4d8555a3d9a4
Zoltan Gabor <Zoltan_Gabor@epam.com>
2020-08-11 15:00:58 +0200

    Use ApiInfoBuilder for Swagger config

--- B31_SpringJPAQuizApp/src/main/kotlin/com/epam/quiz/config/SwaggerConfig.kt
+++ B31_SpringJPAQuizApp/src/main/kotlin/com/epam/quiz/config/SwaggerConfig.kt
@@ -2,6 +2,7 @@ package com.epam.quiz.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.ApiInfo
@@ -15,13 +16,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 class SwaggerConfig {
 
     val contact: Contact = Contact("My contact name", "http://my.contact.url.com", "my.email@address.com")
-    val apiInfo: ApiInfo = ApiInfo("My title", "My description", "1.0", "http://my.terms.of.service.url.com", contact, "My license", "http://my.license.url.com", arrayListOf())
 
     @Bean
     fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
-            .apiInfo(apiInfo)
+            .apiInfo(buildApiInfo())
             .select()
             .apis(RequestHandlerSelectors.basePackage("com.epam.quiz.api"))
             .paths(PathSelectors.any())
+            .build()
+
+    private fun buildApiInfo(): ApiInfo = ApiInfoBuilder()
+            .title("Spring Quiz App title")
+            .description("Some very important description")
+            .version("1.0")
+            .termsOfServiceUrl("http://my.terms.of.service.url.com")
+            .contact(contact)
+            .license("My license")
+            .licenseUrl("http://my.license.url.com")
             .build()
 }
